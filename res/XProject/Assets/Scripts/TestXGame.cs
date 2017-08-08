55c1f4f6abda760cc6f1fbf4033d90678a2f3806
INTRANET\THT <rivertao@123u.com>
2017-08-08 16:24:54 +0800

    [Res]提交项目文件

---
+++ res/XProject/Assets/Scripts/TestXGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XMainClient;
+
+public class TestXGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(XGame.GetXGame(4));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
