68005e7dc50218e6b596d7348c90d9abdcde99dc
INTRANET\THT <rivertao@123u.com>
2017-08-08 16:23:04 +0800

    #更改类库生成后事件，copy dll mdb To项目工程

--- src/XMainClient/XMainClient/XGame.cs
+++ src/XMainClient/XMainClient/XGame.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XMainClient
 {
     public class XGame
     {
-
+        public static int GetXGame(int x=0)
+        {
+            return x * x + 1;
+        }
     }
 }
