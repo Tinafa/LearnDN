dc2b92ee2c449d7e6662831112ecac146828a19b
INTRANET\THT <rivertao@123u.com>
2017-09-05 10:18:55 +0800

    移动DragonBones

--- src/XMainClient/XMainClient/Entity/XFSM.cs
+++ src/XMainClient/XMainClient/Entity/XFSM.cs
@@ -7,22 +7,39 @@ namespace XMainClient
     {
         StateFun _previous = null;
         StateFun _current = null;
+        StateFun _global = null;
+
+        int _preState = -1;
+        int _curState = -1;
+        int _globalState = -1;
 
         private IDictionary<int, StateFun> s_StateMap = new Dictionary<int, StateFun>();
-
-
 
         public void RegisterState(int stateID, StateFun state)
         {
             s_StateMap.Add(stateID, state);
         }
 
-        public void UnRegisterState(int stateID, StateFun state)
+        public void SetGlobalState(int stateID, StateFun state)
+        {
+            _global = state;
+            _globalState = stateID;
+            RegisterState(stateID, state);
+        }
+
+        public void UnRegisterState(int stateID)
         {
             if (s_StateMap.ContainsKey(stateID))
             {
                 s_StateMap.Remove(stateID);
             }
+        }
+
+        public void CancelGlobalState(int stateID)
+        {
+            UnRegisterState(stateID);
+            _global = null;
+            _globalState = -1;
         }
 
         public void ChangeState(int stateID)
@@ -31,22 +48,28 @@ namespace XMainClient
             {
                 _current.Exit();
                 _previous = _current;
+
+                _preState = _curState;
             }
 
             if (s_StateMap.ContainsKey(stateID))
             {
                 _current = s_StateMap[stateID];
                 _current.Enter();
+
+                _curState = stateID;
             }
         }
 
         public virtual void Update(float delta)
         {
+            if (_global != null) _global.Update(delta);
             if (_current != null) _current.Update(delta);
         }
 
         public virtual void FixedUpdate()
         {
+            if (_global != null) _global.FixedUpdate();
             if (_current != null) _current.FixedUpdate();
         }
 
@@ -55,6 +78,11 @@ namespace XMainClient
             s_StateMap.Clear();
             _previous = null;
             _current = null;
+        }
+
+        public void RevertToPreviousState()
+        {
+            ChangeState(_preState);
         }
     }
 }
